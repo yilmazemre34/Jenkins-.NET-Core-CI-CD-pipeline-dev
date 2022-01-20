7045dd75df58acd966d2c4525e1b861e8b769cf4
emre yılmaz <3kturkey@gmail.com>
2022-01-20 22:15:53 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jenkins-.NET-Core-CI-CD-pipeline
+
+[Jenkins with .NET Core CI&CD pipeline for Linux tutorial](https://voltwu.github.io/blog/jenkins/2020/12/30/Jenkins-with-asp-net-core-CI-CD-pipeline-for-linux/)
---
+++ XUnitTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //run unit test code
+            Assert.Equal(1,1);
+        }  
+    }
+}
